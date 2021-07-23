da1c78019ad3f5a6e530f72c1e98dda5336b076a
Seif Lotfy <seif.lotfy@gmail.com>
2021-07-23 16:34:48 +0200

    refactor

--- go.mod
+++ go.mod
@@ -7,5 +7,8 @@ require (
 	github.com/axiomhq/pkg v0.0.0-20210318171555-dc26762456be
 	github.com/golangci/golangci-lint v1.39.0
 	github.com/goreleaser/goreleaser v0.173.2
+	github.com/klauspost/compress v1.12.2
+	github.com/vmihailenco/msgpack/v5 v5.3.4
+	go.uber.org/zap v1.17.0
 	gotest.tools/gotestsum v1.6.3
 )
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,62 +1,173 @@
 package proxy
 
 import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strings"
 
 	"github.com/axiomhq/axiom-go/axiom"
+	"github.com/klauspost/compress/zstd"
+	"github.com/vmihailenco/msgpack/v5"
+	"go.uber.org/zap"
 )
 
-const (
-	honeycombEndpoint = "https://api.honeycomb.io"
-
-	// honeycombPathEvents = "/honeycomb/v1/events/"
-	// honeycombPathBatch  = "/honeycomb/v1/batch/"
-)
-
-var (
-	hcurl *url.URL
-	proxy *httputil.ReverseProxy
-)
+var logger *zap.Logger
 
 func init() {
-	hcurl, _ = url.Parse(honeycombEndpoint)
-	proxy = httputil.NewSingleHostReverseProxy(hcurl)
-}
-
-func GetHandler(client *axiom.Client) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		// Try multiplex
-		multiplex(res, req)
-
-		// Forward to HC
-		forward(res, req)
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
 	}
 }
 
-func forward(res http.ResponseWriter, req *http.Request) {
-	req.URL.Host = hcurl.Host
-	req.URL.Scheme = hcurl.Scheme
-
-	proxy.ServeHTTP(res, req)
-}
-
-func multiplex(res http.ResponseWriter, req *http.Request) {
-	if strings.HasPrefix(req.URL.Path, "/1/events/") {
-		multiplexEvents(res, req)
-	} else if strings.HasPrefix(req.URL.Path, "/1/batch/") {
-		multiplexBatch(res, req)
-	} else {
-		return
+func decompress(rdr io.ReadCloser, encoding string) (io.ReadCloser, error) {
+	switch encoding {
+	case "gzip":
+		decomp, err := gzip.NewReader(rdr)
+		if err != nil {
+			return nil, err
+		}
+		defer decomp.Close()
+		return decomp, nil
+	case "zstd":
+		decomp, err := zstd.NewReader(rdr)
+		if err != nil {
+			return nil, err
+		}
+		return io.NopCloser(decomp), nil
+	default:
+		return rdr, nil
 	}
 }
 
-func multiplexEvents(res http.ResponseWriter, req *http.Request) {
+type Multiplexer struct {
+	client *axiom.Client
+	proxy  *httputil.ReverseProxy
+	hcURL  *url.URL
+}
+
+func NewMultiplexer(client *axiom.Client, honeycombEndpoint string) (*Multiplexer, error) {
+	hcURL, err := url.Parse(honeycombEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	return &Multiplexer{
+		client: client,
+		proxy:  httputil.NewSingleHostReverseProxy(hcURL),
+		hcURL:  hcURL,
+	}, nil
+}
+
+func (m *Multiplexer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	body := bytes.NewBuffer(nil)
+	req.Body = io.NopCloser(io.TeeReader(req.Body, body))
+	m.forward(resp, req)
+	req.Body = io.NopCloser(body)
+	if err := m.multiplex(req); err != nil {
+		logger.Error(err.Error())
+	}
+}
+
+func (m *Multiplexer) forward(resp http.ResponseWriter, req *http.Request) {
+	req.URL.Host = m.hcURL.Host
+	req.URL.Scheme = m.hcURL.Scheme
+	m.proxy.ServeHTTP(resp, req)
+}
+
+func (m *Multiplexer) multiplex(req *http.Request) error {
+	if req.Method != "POST" {
+		return nil
+	}
+
+	body, err := decompress(req.Body, req.Header.Get("Content-Encoding"))
+	if err != nil {
+		return err
+	}
+	req.Body = body
+	defer req.Body.Close()
+
+	switch {
+	case strings.HasPrefix(req.URL.Path, "/1/events/"):
+		return m.multiplexEvents(req)
+	case strings.HasPrefix(req.URL.Path, "/1/batch/"):
+		return m.multiplexBatch(req)
+	default:
+		return nil
+	}
+}
+
+func (m *Multiplexer) multiplexEvents(req *http.Request) error {
+	splitStr := strings.Split(req.URL.Path, "/")
+	dataset := splitStr[len(splitStr)-1]
+
+	ev := axiom.Event{}
+
+	switch req.Header.Get("Content-Type") {
+	case "application/msgpack":
+		if err := msgpack.NewDecoder(req.Body).Decode(&ev); err != nil {
+			return err
+		}
+	default:
+		if err := json.NewDecoder(req.Body).Decode(&ev); err != nil {
+			return err
+		}
+	}
+
+	timeStr := req.Header.Get("X-Honeycomb-Event-Time")
+	if strings.TrimSpace(timeStr) != "" {
+		ev["_time"] = timeStr
+	}
+	return m.sendEvents(req.Context(), dataset, ev)
+}
+
+func (m *Multiplexer) multiplexBatch(req *http.Request) error {
+	splitStr := strings.Split(req.URL.Path, "/")
+	dataset := splitStr[len(splitStr)-1]
+
+	data := make([]map[string]interface{}, 0)
+
+	switch req.Header.Get("Content-Type") {
+	case "application/msgpack":
+		if err := msgpack.NewDecoder(req.Body).Decode(&data); err != nil {
+			return err
+		}
+	default:
+		if err := json.NewDecoder(req.Body).Decode(&data); err != nil {
+			return err
+		}
+	}
+
+	events := make([]axiom.Event, len(data))
+	for i, d := range data {
+		events[i] = d["data"].(map[string]interface{})
+		if timeStr, ok := d["time"].(string); ok {
+			events[i]["_time"] = timeStr
+		}
+	}
+	return m.sendEvents(req.Context(), dataset, events...)
 
 }
 
-func multiplexBatch(res http.ResponseWriter, req *http.Request) {
+func (m *Multiplexer) sendEvents(ctx context.Context, dataset string, events ...axiom.Event) error {
+	opts := axiom.IngestOptions{}
 
+	status, err := m.client.Datasets.IngestEvents(ctx, dataset, opts, events...)
+	if err != nil {
+		return err
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(buf).Encode(status); err != nil {
+		return err
+	}
+
+	logger.Info(buf.String())
+	return nil
 }
