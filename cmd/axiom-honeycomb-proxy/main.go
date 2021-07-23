35b3461c346dd79d61902da4be2fd6b92315bea4
Seif Lotfy <seif.lotfy@gmail.com>
2021-07-23 16:34:48 +0200

    refactor

--- cmd/axiom-honeycomb-proxy/main.go
+++ cmd/axiom-honeycomb-proxy/main.go
@@ -13,9 +13,10 @@ import (
 )
 
 var (
-	deploymentURL = os.Getenv("AXIOM_URL")
-	accessToken   = os.Getenv("AXIOM_TOKEN")
-	addr          = flag.String("addr", ":3111", "Listen address <ip>:<port>")
+	defaultHoneyCombURL = "https://api.honeycomb.io"
+	deploymentURL       = os.Getenv("AXIOM_URL")
+	accessToken         = os.Getenv("AXIOM_TOKEN")
+	addr                = flag.String("addr", ":8080", "Listen address <ip>:<port>")
 )
 
 func main() {
@@ -36,7 +37,11 @@ func main() {
 	}
 
 	log.Print("listening on", *addr)
+	mp, err := proxy.NewMultiplexer(client, defaultHoneyCombURL)
+	if err != nil {
+		panic(err)
+	}
 
-	server := http.Server{Handler: proxy.GetHandler(client), Addr: *addr}
+	server := http.Server{Handler: mp, Addr: *addr}
 	log.Fatal(server.ListenAndServe())
 }
