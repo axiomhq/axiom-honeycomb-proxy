99647910e7429ec476790ce2dc0f02afe61b71c8
Lukas Malkmus <lukas@axiom.co>
2021-09-03 14:26:53 +0200

    Go 1.17 enhancements

--- cmd/axiom-honeycomb-proxy/main.go
+++ cmd/axiom-honeycomb-proxy/main.go
@@ -1,47 +1,92 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/axiomhq/axiom-go/axiom"
+	"github.com/axiomhq/pkg/http"
 	"github.com/axiomhq/pkg/version"
 
 	"github.com/axiomhq/axiom-honeycomb-proxy/proxy"
 )
 
-var (
-	defaultHoneyCombURL = "https://api.honeycomb.io"
-	deploymentURL       = os.Getenv("AXIOM_URL")
-	accessToken         = os.Getenv("AXIOM_TOKEN")
-	addr                = flag.String("addr", ":8080", "Listen address <ip>:<port>")
+const (
+	exitOK int = iota
+	exitConfig
+	exitInternal
 )
 
+const defaultHoneyCombURL = "https://api.honeycomb.io"
+
+var addr = flag.String("addr", ":8080", "Listen address <ip>:<port>")
+
 func main() {
+	os.Exit(Main())
+}
+
+func Main() int {
+	// Export `AXIOM_TOKEN` and `AXIOM_ORG_ID` for Axiom Cloud
+	// Export `AXIOM_URL` and `AXIOM_TOKEN` for Axiom Selfhost
+
 	log.Print("starting axiom-honeycomb-proxy version ", version.Release())
 
 	flag.Parse()
 
-	if deploymentURL == "" {
-		deploymentURL = axiom.CloudURL
-	}
-	if accessToken == "" {
-		log.Fatal("missing AXIOM_TOKEN")
-	}
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		os.Interrupt,
+		os.Kill,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+	)
+	defer cancel()
 
-	client, err := axiom.NewClient(deploymentURL, accessToken)
+	client, err := axiom.NewClient()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return exitConfig
+	} else if err = client.ValidateCredentials(ctx); err != nil {
+		log.Print(err)
+		return exitConfig
 	}
 
-	log.Print("listening on", *addr)
 	mp, err := proxy.NewMultiplexer(client, defaultHoneyCombURL)
 	if err != nil {
-		panic(err)
+		log.Print(err)
+		return exitInternal
 	}
 
-	server := http.Server{Handler: mp, Addr: *addr}
-	log.Fatal(server.ListenAndServe())
+	srv, err := http.NewServer(*addr, mp)
+	if err != nil {
+		log.Print(err)
+		return exitInternal
+	}
+	defer func() {
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer shutdownCancel()
+
+		if shutdownErr := srv.Shutdown(shutdownCtx); shutdownErr != nil {
+			log.Print(shutdownErr)
+		}
+	}()
+
+	srv.Run(ctx)
+
+	log.Print("listening on ", srv.ListenAddr().String())
+
+	select {
+	case <-ctx.Done():
+		log.Print("received interrupt, exiting gracefully")
+	case err := <-srv.ListenError():
+		log.Print("error starting http server, exiting gracefully: ", err)
+		return exitInternal
+	}
+
+	return exitOK
 }
