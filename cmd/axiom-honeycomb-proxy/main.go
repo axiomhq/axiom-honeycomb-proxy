bc9db8b0d1bcc6fa43dbb8cfa31689086f1a421c
Lukas Malkmus <lukas@axiom.co>
2021-09-30 16:08:45 +0200

    Use axiomhq/pkg/cmd

--- cmd/axiom-honeycomb-proxy/main.go
+++ cmd/axiom-honeycomb-proxy/main.go
@@ -3,23 +3,13 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/axiomhq/axiom-go/axiom"
+	"github.com/axiomhq/pkg/cmd"
 	"github.com/axiomhq/pkg/http"
-	"github.com/axiomhq/pkg/version"
+	"go.uber.org/zap"
 
 	"github.com/axiomhq/axiom-honeycomb-proxy/proxy"
-)
-
-const (
-	exitOK int = iota
-	exitConfig
-	exitInternal
 )
 
 const defaultHoneyCombURL = "https://api.honeycomb.io"
@@ -27,66 +17,49 @@ const defaultHoneyCombURL = "https://api.honeycomb.io"
 var addr = flag.String("addr", ":8080", "Listen address <ip>:<port>")
 
 func main() {
-	os.Exit(Main())
+	cmd.Run("axiom-honeycomb-proxy", run,
+		cmd.WithValidateAxiomCredentials(),
+	)
 }
 
-func Main() int {
-	// Export `AXIOM_TOKEN` and `AXIOM_ORG_ID` for Axiom Cloud
-	// Export `AXIOM_URL` and `AXIOM_TOKEN` for Axiom Selfhost
-
-	log.Print("starting axiom-honeycomb-proxy version ", version.Release())
+func run(ctx context.Context, log *zap.Logger, client *axiom.Client) error {
+	// Export `AXIOM_TOKEN` and `AXIOM_ORG_ID` for Axiom Cloud.
+	// Export `AXIOM_URL` and `AXIOM_TOKEN` for Axiom Selfhost.
 
 	flag.Parse()
 
-	ctx, cancel := signal.NotifyContext(context.Background(),
-		os.Interrupt,
-		os.Kill,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGQUIT,
-	)
-	defer cancel()
-
-	client, err := axiom.NewClient()
-	if err != nil {
-		log.Print(err)
-		return exitConfig
-	} else if err = client.ValidateCredentials(ctx); err != nil {
-		log.Print(err)
-		return exitConfig
-	}
-
 	mp, err := proxy.NewMultiplexer(client, defaultHoneyCombURL)
 	if err != nil {
-		log.Print(err)
-		return exitInternal
+		return cmd.Error("create multiplexer", err)
 	}
 
-	srv, err := http.NewServer(*addr, mp)
+	srv, err := http.NewServer(*addr, mp,
+		http.WithBaseContext(ctx),
+		http.WithLogger(log),
+	)
 	if err != nil {
-		log.Print(err)
-		return exitInternal
+		return cmd.Error("create http server", err)
 	}
 	defer func() {
-		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*5)
-		defer shutdownCancel()
-
-		if shutdownErr := srv.Shutdown(shutdownCtx); shutdownErr != nil {
-			log.Print(shutdownErr)
+		if shutdownErr := srv.Shutdown(); shutdownErr != nil {
+			log.Error("stopping server", zap.Error(shutdownErr))
+			return
 		}
 	}()
 
 	srv.Run(ctx)
 
-	log.Print("listening on ", srv.ListenAddr().String())
+	log.Info("server listening",
+		zap.String("address", srv.ListenAddr().String()),
+		zap.String("network", srv.ListenAddr().Network()),
+	)
 
 	select {
 	case <-ctx.Done():
-		log.Print("received interrupt, exiting gracefully")
+		log.Warn("received interrupt, exiting gracefully")
 	case err := <-srv.ListenError():
-		log.Print("error starting http server, exiting gracefully: ", err)
-		return exitInternal
+		return cmd.Error("error starting http server, exiting gracefully", err)
 	}
 
-	return exitOK
+	return nil
 }
