eada87f217124bcadc742dd9d111e1a0ff2231ca
Lukas Malkmus <lukas@axiom.co>
2024-06-12 11:42:43 +0200

    chore: update dependencies

--- tools.go
+++ tools.go
@@ -4,6 +4,6 @@ package axiomhoneycombproxy
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/goreleaser/goreleaser/v2"
 	_ "gotest.tools/gotestsum"
 )
